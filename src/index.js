26f07d130b6a1f52f059fddaf4a41e2a7f475876
Jordan Butterfield <jmcbutter@gmail.com>
2022-09-23 15:41:44 -0500

    About to start really messing with styles for responsive design

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 let form = document.getElementsByClassName("email")[0];
+let emailInput = document.getElementById("email");
+
+emailInput.addEventListener("input", () => {
+  emailInput.setCustomValidity("");
+  emailInput.checkValidity();
+})
+
+emailInput.addEventListener("invalid", () => {
+  if (emailInput.value === "") {
+    emailInput.setCustomValidity("Please Enter an Email!");
+  } else {
+    emailInput.setCustomValidity("Please enter a valid email (example: email@email.com)")
+  }
+})
 
 form.addEventListener("submit", (e) => {
-  alert("Thank you for signing up!");
+  e.preventDefault();
+  alert("Thank you for signing up " + emailInput.value + "!");
 });
-
-
